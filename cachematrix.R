6cd449e0dea4cb2f1a5b2f06631ca0c17f46cea0
IM-data-sci <sunilupdt@aol.com>
2014-07-24 23:29:50 -0400

    hopefully accounts for matrix changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,33 @@
 ## The functions below cache a matrix and return its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-      ## caches a matrix, calculates its  
-      ## inverse, returns both in a list
-      i <- NULL
-      set <- function(y) {
-            x <<- y
-            i <<- NULL
-      }
-      get <- function() x
-      setinv <- function(solve) i <<- solve
-      getinv <- function() solve(x)
-      list(set = set, get = get, setinv = setinv, getinv = getinv)
+  ## caches a matrix, calculates its  
+  ## inverse, returns both in a list
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- solve(x) i <<- solve
+  getinv <- function() i
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-cacheSolve <- function(x = matrix(), ...) {
-      ## calculates matrix inverse 
-      ## (if not done) and returns it.
-      i <- x$getinv()
-      j <- x$get()
-      if(!is.null(i)) {
-#            if(identical(j,x)) {
-                  message("getting cached data")
-                  return(i)  
-#            }
-      }
-      data <- x$get()
-      i <- solve(data, ...)
-      x$setinv(i)
-      i
+cacheSolve <- function(x, ...) {
+  ## calculates matrix inverse 
+  ## (if not done) and returns it.
+  i <- x$getinv()
+  j <- x$get()
+  
+  if(isTRUE(all.equal(x$get(), j)) == TRUE) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  message("the matrix has changed")
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
